42220a6942d1519069a7576a4465cb7fef06cffe
Ibrahim-Ebaid <ibrahimebaid12@gmail.com>
2026-02-13 02:11:33 +0200

    edit route name

--- src/pages/api/get-cart/route.ts
+++ src/pages/api/get-cart/route.ts
@@ -2,7 +2,7 @@ import { getUserToken } from "@/app/(pages)/Helpers/getUserToken";
 import { CartResponse } from "@/interfaces";
 import { NextResponse } from "next/server";
 
-export async function Get() {
+export async function GET() {
   const token = getUserToken();
   const response = await fetch("https://ecommerce.routemisr.com/api/v1/cart", {
     headers: {
